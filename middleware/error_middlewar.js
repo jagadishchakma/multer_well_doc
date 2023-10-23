4e64e8e255d879fd72bac52172fcbd644ec187f2
jagadishchakma <jagadishchakma401@gmail.com>
2023-10-23 23:31:55 +0600

    error add

--- middleware/error_middlewar.js
+++ middleware/error_middlewar.js
@@ -1,7 +1,7 @@
 function error_middleware(err,req,res,next){
 
     if(err){
-        res.render("pages/error.ejs",{err});
+        res.render("pages/error.ejs");
     }else{
         res.send(new Error("Error Occured"));
     }
